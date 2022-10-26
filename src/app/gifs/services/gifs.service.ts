6e5c427cd74a7ac17472ef12f40571756247d447
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-10-26 18:42:43 -0300

    Configuraciones Gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { GIFSearch, Gifs } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apikey: string = 'w703POlhQTfwMTTsTEE6QUepWDbIXTqB';
+
   private _historial: string[] = [];
+
+  //TODO: cambiar any por su tipo
+  public resultados: Gifs[] = [];
 
   get historial(){
     return [ ...this._historial];
   }
+
+  constructor(private httpClient: HttpClient){}
 
   buscarGifs( query: string = ''){
 
@@ -21,7 +30,11 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }
 
-    
+    this.httpClient.get<GIFSearch>(`https://api.giphy.com/v1/gifs/search?api_key=w703POlhQTfwMTTsTEE6QUepWDbIXTqB&q=${ query }&limit=10`)
+              .subscribe(resp =>{
+                console.log(resp);
+                this.resultados = resp.data;
+              })    
 
   }
 }
